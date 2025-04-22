b5340acad4de245dfc61a04bb2e7c852fae09437
Emmaqsito <jesrex50@gmail.com>
2025-04-22 09:18:50 -0600

    Matriz 1 (arquigay)

---
+++ ArrayBidimensional.java
@@ -0,0 +1,35 @@
+public class ArrayBidimensional {
+    // se puede imaginar un arreglo bidimensional como una tabla que tiene cierto
+    // numero de renglones y
+    // columnas, o como un plano con una combinacion de coordenadas x,y.
+    // al tratar un arreglo que tiene dos dimensiones
+    // se requieren de dos indices para identificar a cada elemento dentro de el.
+    // uno de los indices indica el renglon y el seguno indica la columna
+    public static void main(String[] args) {
+        int sum = 0;
+        int x = 0;
+        int a = 0;
+        int b = 0;
+        int[][] matriz = {
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+                { 7, 8, 9 }
+        };
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                sum = sum + matriz[i][j];
+            }
+            System.out.println("Fila " + i + " " + sum);
+            if (i == 0) {
+                sum = x;
+                sum = 0;
+            } else if (i == 1) {
+                sum = a;
+                sum = 0;
+            } else if (i == 2) {
+                sum = b;
+                sum = 0;
+            }
+        }
+    }
+}
